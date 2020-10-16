6e54ef92ab840bc87aa6d080c5925e20c63f47e0
kthr3e <kimiyuki001@gmail.com>
2020-10-17 00:17:27 +0900

    ubuntu18.04

---
+++ Tello_python/Tello_Video/h264decoder/build/CMakeFiles/h264decoder.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/kthree/Tello-Python/Tello_Video/h264decoder/h264decoder.cpp" "/home/kthree/Tello-Python/Tello_Video/h264decoder/build/CMakeFiles/h264decoder.dir/h264decoder.cpp.o"
+  "/home/kthree/Tello-Python/Tello_Video/h264decoder/h264decoder_python.cpp" "/home/kthree/Tello-Python/Tello_Video/h264decoder/build/CMakeFiles/h264decoder.dir/h264decoder_python.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "h264decoder_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/python2.7"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")

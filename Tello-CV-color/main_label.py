4287f40e5dd36e301f5fcdc739c97e8ec5100fe9
kthr3e <kimiyuki001@gmail.com>
2020-10-30 21:27:15 +0900

    hsv二値化にくわえて、ラベリング処理と処理結果表示を追加

--- Tello-CV-color/main_label.py
+++ Tello-CV-color/main_label.py
@@ -17,6 +17,19 @@ def main():
 	# トラックバーを作るため，まず最初にウィンドウを生成
 	cv2.namedWindow("OpenCV Window")
 
+	# トラックバーのコールバック関数は何もしない空の関数
+	def nothing(x):
+		pass
+
+	# トラックバーの生成
+	cv2.createTrackbar("H_min", "OpenCV Window", 0, 179, nothing)
+	cv2.createTrackbar("H_max", "OpenCV Window", 9, 179, nothing)       # Hueの最大値は179
+	cv2.createTrackbar("S_min", "OpenCV Window", 128, 255, nothing)
+	cv2.createTrackbar("S_max", "OpenCV Window", 255, 255, nothing)
+	cv2.createTrackbar("V_min", "OpenCV Window", 128, 255, nothing)
+	cv2.createTrackbar("V_max", "OpenCV Window", 255, 255, nothing)
+
+
 	#Ctrl+cが押されるまでループ
 	try:
 		while True:
@@ -28,11 +41,56 @@ def main():
 
 			# (B)ここから画像処理
 			image = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)		# OpenCV用のカラー並びに変換する
-			small_image = cv2.resize(image, dsize=(960,720) )	# 画像サイズを半分に変更
+			bgr_image = cv2.resize(image, dsize=(960,720) )	# 画像サイズを半分に変更
 
+			hsv_image = cv2.cvtColor(bgr_image, cv2.COLOR_BGR2HSV)  # BGR画像 -> HSV画像
+
+			# トラックバーの値を取る
+			h_min = cv2.getTrackbarPos("H_min", "OpenCV Window")
+			h_max = cv2.getTrackbarPos("H_max", "OpenCV Window")
+			s_min = cv2.getTrackbarPos("S_min", "OpenCV Window")
+			s_max = cv2.getTrackbarPos("S_max", "OpenCV Window")
+			v_min = cv2.getTrackbarPos("V_min", "OpenCV Window")
+			v_max = cv2.getTrackbarPos("V_max", "OpenCV Window")
+
+			# inRange関数で範囲指定２値化
+			bin_image = cv2.inRange(hsv_image, (h_min, s_min, v_min), (h_max, s_max, v_max)) # HSV画像なのでタプルもHSV並び
+
+			# bitwise_andで元画像にマスクをかける　ー＞　マスクされた部分の色だけ残る
+			masked_image = cv2.bitwise_and(hsv_image,hsv_image, mask=bin_image)
+
+			# ラベリング結果書き出し用に画像を準備
+			out_image = masked_image
+
+			# 面積・重心計算付きのラベリング処理を行う
+			num_labels,label_image,stats,center = cv2.connectedComponentsWithStats(bin_image)
+
+			# 最大のラベルは画面全体を覆う黒なので不要。黒のデータを削除する。
+			num_labels  =num_labels - 1
+			stats = np.delete(stats,0,0)
+			center = np.delete(center,0,0)
+
+			# 検出したラベルの数だけ繰り返す
+			for index in range(num_labels):
+				# ラベルのx,y,w,h,面積s,重心位置mx,myを取り出す
+				x = stats[index][0]
+				y = stats[index][1]
+				w = stats[index][2]
+				h = stats[index][3]
+				s = stats[index][4]
+				mx = int(center[index][0])
+				my = int(center[index][1])
+				#print("(x,y)=%d,%d (w,h)=%d,%d s=%d (mx,my)=%d,%d"%(x, y, w, h, s, mx, my) )
+
+				# ラベルを加工バウンディングボックスを描画
+				cv2.rectangle(out_image,(x,y),(x+w,y+h),(255,0,255))
+
+				# 重心位置の座標と面積を表示
+				cv2.putText(out_image,"%d,%d"%(mx,my), (x-15, y+h+15), cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 0))
+				cv2.putText(out_image, "%d"%(s), (x, y+h+30), cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 0))
 
 			# (X)ウィンドウに表示
-			cv2.imshow('OpenCV Window', small_image)	# ウィンドウに表示するイメージを変えれば色々表示できる
+			cv2.imshow('OpenCV Window', out_image)	# ウィンドウに表示するイメージを変えれば色々表示できる
 
 			# (Y)OpenCVウィンドウでキー入力を1ms待つ
 			key = cv2.waitKey(1)

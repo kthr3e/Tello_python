d4c0c0b86f928cd2f11b087a7c047f5240dc6d52
kthr3e <kimiyuki001@gmail.com>
2020-11-02 21:10:51 +0900

    hsv検出で追跡するやつしてみたけど色がたくさんあると使えん

--- Tello-CV-color/main_control.py
+++ Tello-CV-color/main_control.py
@@ -8,7 +8,7 @@ import numpy as np
 # メイン関数
 def main():
 	# Telloクラスを使って，droneというインスタンス(実体)を作る
-	drone = tello.Tello('', 8889, command_timeout=.01)
+	drone = tello.Tello('', 8889, command_timeout=.01)  
 
 	current_time = time.time()	# 現在時刻の保存変数
 	pre_time = current_time		# 5秒ごとの'command'送信のための時刻変数
@@ -24,13 +24,13 @@ def main():
 
 	# トラックバーの生成
 	cv2.createTrackbar("H_min", "OpenCV Window", 0, 179, nothing)
-	cv2.createTrackbar("H_max", "OpenCV Window", 9, 179, nothing)       # Hueの最大値は179
+	cv2.createTrackbar("H_max", "OpenCV Window", 9, 179, nothing)		# Hueの最大値は179
 	cv2.createTrackbar("S_min", "OpenCV Window", 128, 255, nothing)
 	cv2.createTrackbar("S_max", "OpenCV Window", 255, 255, nothing)
 	cv2.createTrackbar("V_min", "OpenCV Window", 128, 255, nothing)
 	cv2.createTrackbar("V_max", "OpenCV Window", 255, 255, nothing)
 
-
+	flag = 0
 	#Ctrl+cが押されるまでループ
 	try:
 		while True:
@@ -38,13 +38,13 @@ def main():
 			# (A)画像取得
 			frame = drone.read()	# 映像を1フレーム取得
 			if frame is None or frame.size == 0:	# 中身がおかしかったら無視
-				continue
+				continue 
 
 			# (B)ここから画像処理
 			image = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)		# OpenCV用のカラー並びに変換する
-			bgr_image = cv2.resize(image, dsize=(960,720) )	# 画像サイズを半分に変更
+			bgr_image = cv2.resize(image, dsize=(480,360) )	# 画像サイズを半分に変更
 
-			hsv_image = cv2.cvtColor(bgr_image, cv2.COLOR_BGR2HSV)  # BGR画像 -> HSV画像
+			hsv_image = cv2.cvtColor(bgr_image, cv2.COLOR_BGR2HSV)	# BGR画像 -> HSV画像
 
 			# トラックバーの値を取る
 			h_min = cv2.getTrackbarPos("H_min", "OpenCV Window")
@@ -57,38 +57,60 @@ def main():
 			# inRange関数で範囲指定２値化
 			bin_image = cv2.inRange(hsv_image, (h_min, s_min, v_min), (h_max, s_max, v_max)) # HSV画像なのでタプルもHSV並び
 
-			# bitwise_andで元画像にマスクをかける　ー＞　マスクされた部分の色だけ残る
-			masked_image = cv2.bitwise_and(hsv_image,hsv_image, mask=bin_image)
+			# bitwise_andで元画像にマスクをかける -> マスクされた部分の色だけ残る
+			masked_image = cv2.bitwise_and(hsv_image, hsv_image, mask=bin_image)
 
 			# ラベリング結果書き出し用に画像を準備
 			out_image = masked_image
 
 			# 面積・重心計算付きのラベリング処理を行う
-			num_labels,label_image,stats,center = cv2.connectedComponentsWithStats(bin_image)
+			num_labels, label_image, stats, center = cv2.connectedComponentsWithStats(bin_image)
 
-			# 最大のラベルは画面全体を覆う黒なので不要。黒のデータを削除する。
-			num_labels  =num_labels - 1
-			stats = np.delete(stats,0,0)
-			center = np.delete(center,0,0)
+			# 最大のラベルは画面全体を覆う黒なので不要．データを削除
+			num_labels = num_labels - 1
+			stats = np.delete(stats, 0, 0)
+			center = np.delete(center, 0, 0)
 
-			# 検出したラベルの数だけ繰り返す
-			for index in range(num_labels):
-				# ラベルのx,y,w,h,面積s,重心位置mx,myを取り出す
-				x = stats[index][0]
-				y = stats[index][1]
-				w = stats[index][2]
-				h = stats[index][3]
-				s = stats[index][4]
-				mx = int(center[index][0])
-				my = int(center[index][1])
+
+			if num_labels >= 1:
+				# 面積最大のインデックスを取得
+				max_index = np.argmax(stats[:,4])
+				#print max_index
+
+				# 面積最大のラベルのx,y,w,h,面積s,重心位置mx,myを得る
+				x = stats[max_index][0]
+				y = stats[max_index][1]
+				w = stats[max_index][2]
+				h = stats[max_index][3]
+				s = stats[max_index][4]
+				mx = int(center[max_index][0])
+				my = int(center[max_index][1])
 				#print("(x,y)=%d,%d (w,h)=%d,%d s=%d (mx,my)=%d,%d"%(x, y, w, h, s, mx, my) )
 
-				# ラベルを加工バウンディングボックスを描画
-				cv2.rectangle(out_image,(x,y),(x+w,y+h),(255,0,255))
+				# ラベルを囲うバウンディングボックスを描画
+				cv2.rectangle(out_image, (x, y), (x+w, y+h), (255, 0, 255))
 
-				# 重心位置の座標と面積を表示
-				cv2.putText(out_image,"%d,%d"%(mx,my), (x-15, y+h+15), cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 0))
-				cv2.putText(out_image, "%d"%(s), (x, y+h+30), cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 0))
+				# 重心位置の座標を表示
+				#cv2.putText(out_image, "%d,%d"%(mx,my), (x-15, y+h+15), cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 0))
+				cv2.putText(out_image, "%d"%(s), (x, y+h+15), cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 0))
+
+				if flag == 1:
+					a = b = c = d = 0
+
+          # P制御の式(Kpゲインはとりあえず1.0)
+					dx = 1.0 * (240 - mx)		# 画面中心との差分
+
+					# 旋回方向の不感帯を設定
+					d = 0.0 if abs(dx) < 50.0 else dx   # ±50未満ならゼロにする
+
+					d = -d
+					# 旋回方向のソフトウェアリミッタ(±100を超えないように)
+					d =  100 if d >  100.0 else d
+					d = -100 if d < -100.0 else d
+
+					print('dx=%f'%(dx) )
+					drone.send_command('rc %s %s %s %s'%(int(a), int(b), int(c), int(d)) )
+
 
 			# (X)ウィンドウに表示
 			cv2.imshow('OpenCV Window', out_image)	# ウィンドウに表示するイメージを変えれば色々表示できる
@@ -117,6 +139,10 @@ def main():
 				drone.move_up(0.3)			# 上昇
 			elif key == ord('f'):
 				drone.move_down(0.3)		# 下降
+			elif key == ord('1'):
+				flag = 1					# 追跡モードON
+			elif key == ord('2'):
+				flag = 0					# 追跡モードOFF
 
 			# (Z)5秒おきに'command'を送って、死活チェックを通す
 			current_time = time.time()	# 現在時刻を取得
@@ -127,6 +153,7 @@ def main():
 	except( KeyboardInterrupt, SystemExit):    # Ctrl+cが押されたら離脱
 		print( "SIGINTを検知" )
 
+	drone.send_command('streamoff')
 	# telloクラスを削除
 	del drone
 
